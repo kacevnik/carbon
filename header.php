d8aeb9493b044a432652196353efc36a99cd3265
kacevnik <kacevnik@yandex.ru>
2017-06-19 21:18:14 +0300

    Верстка хедера

--- header.php
+++ header.php
@@ -12,8 +12,27 @@
 </head>
 <body <?php body_class(); ?>>
 	<header>
-		<?php
-			$args = array('theme_location' => 'top', 'container'=> 'nav', 'menu_class' => 'bottom-menu', 'menu_id' => 'bottom-nav');
+		<div class="container">
+			<div class="row">
+				<div class="header_left">
+					<?php
+						$args = array('theme_location' => 'top', 'container'=> 'nav', 'menu_class' => 'top-menu', 'menu_id' => 'top-nav');
 			wp_nav_menu($args);
-		?>
+					?>
+					<div class="header_logo">
+						<a href="#">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png">
+						</a>
+					</div>
+				</div>
+				<div class="header_right">
+					<ul class="header_right_list">
+						<li class="header_cart">$2,399<i class="fa fa-shopping-cart" aria-hidden="true"></i><span>8</span></li>
+						<li class="header_favorites"><i class="fa fa-heart-o" aria-hidden="true"></i><span>10</span></li>
+						<li class="header_login"><a href="#"><i class="fa fa-user" aria-hidden="true"></i>You</a></li>
+						<li class="header_lang"><a href="">RU</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
 	</header>
--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 jQuery(document).ready(function ($) {		
-	$('.section_3').css({'background-position-x': ($(window).width() - 1200)/2 -365 + 'px'});
+	$('.header_left').append('<div class="bottom_line"></div>');
+	$('.bottom_line').width($('.top-menu >.current-menu-item').width()-20+'px');
 
-	$('.footer_menu ul').css({'margin-left': ($('.footer_menu').width()-$('.footer_menu ul').width())/2+'px'});
+	$('.top-menu >.menu-item').hover(function(){
+		var a = $(this).offset();
+		var t = $('.top-menu').offset();
+		$('.bottom_line').stop().animate({'width': $(this).width()-20+'px', 'left': a.left-t.left}, 100);
+	});
+
+	$('.top-menu').mouseleave(function(){
+		var c = $('.top-menu >.current-menu-item').width()-20+'px';
+		var a = $('.top-menu >.current-menu-item').offset();
+		var t = $('.top-menu').offset();
+		$('.bottom_line').stop().animate({width: c, left: a.left-t.left}, 100);
+	});
+	
 });
