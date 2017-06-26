52a37666d3f4728ed8d56d2bef85f5e6d53219d5
kacevnik <kacevnik@yandex.ru>
2017-06-26 12:42:27 +0300

    Добавил блок Новые ПРодукты с версткой

--- front-page.php
+++ front-page.php
@@ -84,4 +84,145 @@
 		</div>
 	</div>
 </section>
+<section class="new_product_front">
+	<div class="container">
+		<h1>New Products</h1>
+		<div class="row">
+			<div class="col-md-3">
+				<div class="new_priduct_front_item">
+					<div class="new_product_item_img">
+						<a href="">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/car6.jpg" alt="">
+						</a>
+					</div>
+					<div class="new_priduct_front_item_body">
+					<h6>Front Spoiler</h6>
+						<ul>
+							<li>100% carbon</li>
+							<li>Fits any G-lass</li>
+							<li>Doesn't include pipes</li>
+							<li>Doesn't include pipes</li>
+							<li>Doesn't include pipes</li>
+							<div class="new_product_item_status">2 weeks</div>
+						</ul>
+						<div class="new_product_item_bottom">
+							<a href="" class="new_product_item_bottom_price">$ 5 499</a>
+							<a href="" class="new_product_item_bottom_key"><i class="fa fa-wrench"></i>353</a>
+							<div class="new_product_item_bottom_right">
+								<a href="" class="new_product_item_bottom_share">
+									<i class="fa fa-bookmark"></i>
+								</a>
+								<a href="" class="new_product_item_bottom_mem">
+									<i class="fa fa-heart-o"></i>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-md-3">
+				<div class="new_priduct_front_item">
+					<div class="new_product_item_img">
+						<a href="">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/car6.jpg" alt="">
+						</a>
+					</div>
+					<div class="new_priduct_front_item_body">
+					<h6>Front Spoiler</h6>
+						<ul>
+							<li>100% carbon</li>
+							<li>Fits any G-lass</li>
+							<li>Doesn't include pipes</li>
+							<li>Doesn't include pipes</li>
+							<li>Doesn't include pipes</li>
+							<div class="new_product_item_status">2 weeks</div>
+						</ul>
+						<div class="new_product_item_bottom">
+							<a href="" class="new_product_item_bottom_price">$ 5 499</a>
+							<a href="" class="new_product_item_bottom_key"><i class="fa fa-wrench"></i>353</a>
+							<div class="new_product_item_bottom_right">
+								<a href="" class="new_product_item_bottom_share">
+									<i class="fa fa-bookmark"></i>
+								</a>
+								<a href="" class="new_product_item_bottom_mem">
+									<i class="fa fa-heart-o"></i>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="new_priduct_front_item">
+					<div class="new_product_item_img">
+						<a href="">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/car6.jpg" alt="">
+						</a>
+					</div>
+					<div class="new_priduct_front_item_body">
+					<h6>Front Spoiler</h6>
+						<ul>
+							<li>100% carbon</li>
+							<li>Fits any G-lass</li>
+							<li>Doesn't include pipes</li>
+							<li>Doesn't include pipes</li>
+							<li>Doesn't include pipes</li>
+							<div class="new_product_item_status">2 weeks</div>
+						</ul>
+						<div class="new_product_item_bottom">
+							<a href="" class="new_product_item_bottom_price">$ 5 499</a>
+							<a href="" class="new_product_item_bottom_key"><i class="fa fa-wrench"></i>353</a>
+							<div class="new_product_item_bottom_right">
+								<a href="" class="new_product_item_bottom_share">
+									<i class="fa fa-bookmark"></i>
+								</a>
+								<a href="" class="new_product_item_bottom_mem">
+									<i class="fa fa-heart-o"></i>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3">
+				<div class="new_priduct_front_item">
+					<div class="new_product_item_img">
+						<a href="">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/car6.jpg" alt="">
+						</a>
+					</div>
+					<div class="new_priduct_front_item_body">
+					<h6>Front Spoiler</h6>
+						<ul>
+							<li>100% carbon</li>
+							<li>Fits any G-lass</li>
+							<li>Doesn't include pipes</li>
+							<li>Doesn't include pipes</li>
+							<li>Doesn't include pipes</li>
+							<div class="new_product_item_status">2 weeks</div>
+						</ul>
+						<div class="new_product_item_bottom">
+							<a href="" class="new_product_item_bottom_price">$ 5 499</a>
+							<a href="" class="new_product_item_bottom_key"><i class="fa fa-wrench"></i>353</a>
+							<div class="new_product_item_bottom_right">
+								<a href="" class="new_product_item_bottom_share">
+									<i class="fa fa-bookmark"></i>
+								</a>
+								<a href="" class="new_product_item_bottom_mem">
+									<i class="fa fa-heart-o"></i>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="learn_more_new_products">
+				<a href="">Learn More<br><i class="fa fa-plus-circle"></i></a>
+			</div>
+		</div>
+	</div>
+</section>
 <?php get_footer(); ?>
